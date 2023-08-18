a1b9a6e142e87cfcf4ce350737c23e2d59f321fe
Karkuvel Pandi <karkuvelpandi007@gmail.com>
2023-08-18 21:50:46 +0530

    feat: :tada: starts a new project

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx,js,jsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
